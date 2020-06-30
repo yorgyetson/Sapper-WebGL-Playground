399a08d0b956322ba10aa03237f599cd95f746b5
Yorg <yorgyetson@gmail.com>
2020-06-30 04:31:52 -0600

    fix tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,9 +8,4 @@
 
 <h1>Great success!</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong class="text-white">Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
